77d1ebf68a7d7d5ae2a69c7e6fd30cddd54b6283
manontanto <manontanto@gmail.com>
2020-05-27 16:53:40 +0900

    createdb.pyで実行の許可を求める

--- createdb.py
+++ createdb.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 # createdb.py
-# 2020-05-24
+# 2020-05-24, 27
 import sqlite3
+import sys
 
 dbname = 'index.db'
 conn = sqlite3.connect(dbname)
 c = conn.cursor()
+rep = input('索引データベースを初期化します. いいですね: Yes/No')
+if rep == 'N' or rep == 'No':
+    sys.exit()
+
 try:
     ddl = '''
     CREATE TABLE list
