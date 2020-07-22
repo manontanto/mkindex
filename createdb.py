ca6d65877d3bfc74597596c776967218b94f6ab9
manontanto <manontanto@gmail.com>
2020-07-22 14:36:41 +0900

    Add edit db data

--- createdb.py
+++ createdb.py
@@ -5,14 +5,21 @@ import sqlite3
 import sys
 
 dbname = 'index.db'
+dt = datetime.today().strftime('%Y%m%d%H%M')
+
 conn = sqlite3.connect(dbname)
 c = conn.cursor()
-rep = input('索引データベースを初期化します. いいですね: Yes/No')
+rep = input('索引データベースを初期化します. OK?: [Y/n]')
 if rep == 'N' or rep == 'No':
     sys.exit()
 
 try:
     ddl = '''
+    CREATE TABLE id
+     (
+        id TEXT NOT NULL
+        );
+    INSERT INTO id VALUES('{}');
     CREATE TABLE list
      (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -21,7 +28,7 @@ try:
         remark TEXT
     );
      '''
-    c.execute(ddl)
+    c.execute(ddl.format(dt))
 except sqlite3.Error as e:
     print('sqlite3.Error occurred:', e.args[0])
     
